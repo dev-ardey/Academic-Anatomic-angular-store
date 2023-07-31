9a662850e5e7e9af042b66c6f5d16263b52bacbd
dev-ardey <mna.ankrah@gmail.com>
2023-07-31 16:14:39 +0200

    added sort basics

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: 'products-header.component.html',
+})
+export class ProductsHeaderComponent implements OnInit {
+  sort = 'descending';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // updrates sort
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+}
