ed1d33830c9c182b499b8d0d32a8bfcd15e05902
dev-ardey <mna.ankrah@gmail.com>
2023-08-01 12:03:37 +0200

    Added component functionality for header column

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: 'products-header.component.html',
 })
 export class ProductsHeaderComponent implements OnInit {
+  // output sends data outside of component to parent component
+  @Output() columnsCountChange = new EventEmitter<number>();
   sort = 'descending';
+  itemsShowCount = 13;
 
   constructor() { }
 
@@ -15,5 +18,15 @@ export class ProductsHeaderComponent implements OnInit {
   // updrates sort
   onSortUpdated(newSort: string): void {
     this.sort = newSort;
+  }
+
+  //doesnt return anything just returns the value
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    //call columnsCountChange and emit that number
+    this.columnsCountChange.emit(colsNum);
   }
 }
