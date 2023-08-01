b5f04e02b7f4a3dedd514ad851563708f81beca4
dev-ardey <mna.ankrah@gmail.com>
2023-08-01 14:31:24 +0200

    finished grid tile basic

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html',
 })
 export class ProductBoxComponent implements OnInit {
+  // default is 3 product boxes per row, but if user clicks on first sorting icon, it will change to fullWidthMode
+  @Input() fullWidthMode = false;
 
   constructor() { }
 
