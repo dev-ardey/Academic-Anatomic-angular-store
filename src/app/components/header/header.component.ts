979659df240c4c3f4c3e2e90da98e8e25d7944ce
dev-ardey <mna.ankrah@gmail.com>
2023-08-14 15:51:31 +0200

    added cart display functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+
+  private _cart: Cart = { items: []};
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+
+  set cart(cart: Cart) {
+    this._cart = cart;
+
+    // instead of objects, only change quantity
+    this.itemsQuantity = cart.items
+      .map((item) => item.quantity)
+      // then add items up
+      .reduce((prev, current) => prev + current, 0);
+  }
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
